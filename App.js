32d50d7e9368cd75af75dfc08fc98f3c0a78bff9
Applebeef <58487936+Applebeef@users.noreply.github.com>
2022-01-31 18:45:14 +0200

    Map starts on user location + started work on geocoding for dog parks

--- App.js
+++ App.js
@@ -3,18 +3,38 @@ import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import MapContainer from "./components/Map";
 import Title from "./components/Title";
 import ButtonsContainer from "./components/Buttons";
+import { useState, useEffect } from "react";
+import * as Location from "expo-location";
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  const key = "AIzaSyAHx3VwmotBdOTNMMup8VE5ZoTYC1aGQkA";
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.title}>
         <Title />
       </View>
       <View style={styles.map}>
-        <MapContainer />
+        <MapContainer location={location} apiKey={key} />
       </View>
       <View style={styles.buttonsContainer}>
-        <ButtonsContainer/>
+        <ButtonsContainer />
       </View>
     </View>
   );
@@ -33,7 +53,7 @@ const styles = StyleSheet.create({
     top: 30,
   },
   map: {},
-  buttonsContainer:{
+  buttonsContainer: {
     position: "absolute",
     bottom: 10,
   },
--- components/Map.js
+++ components/Map.js
@@ -1,10 +1,33 @@
-import { StyleSheet, View, Image, Dimensions } from "react-native";
+import { useState } from "react";
+import { StyleSheet, View, Dimensions } from "react-native";
 import MapView from "react-native-maps";
+import Geocoder from "react-native-geocoding";
 
-const MapContainer = () => {
+const MapContainer = (props) => {
+  Geocoder.init(props.apiKey); // TODO restrict api key. 
+
+  let list = Geocoder.from("dog park").then((json) => {//FIXME
+    var location = json.results[0].geometry.location;
+    console.log(location);
+  });
+
+  const convertLocationToRegion = () => {
+    return {
+      latitude: props.location?.coords.latitude,
+      longitude: props.location?.coords.longitude,
+      latitudeDelta: 0.015,
+      longitudeDelta: 0.015,
+    };
+  };
+
   return (
     <View style={styles.mapContainer}>
-      <MapView style={styles.map}></MapView>
+      <MapView
+        region={convertLocationToRegion()}
+        showsUserLocation={true}
+        showsPointsOfInterest={false}
+        style={styles.map}
+      ></MapView>
     </View>
   );
 };
