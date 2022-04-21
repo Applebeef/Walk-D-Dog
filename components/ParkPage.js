e092bf211f6dec135ac2714d8c161be13d8dca87
Applebeef <58487936+Applebeef@users.noreply.github.com>
2022-04-21 18:29:36 +0300

    work on ParkPage and added VisitorDisplay

--- components/ParkPage.js
+++ components/ParkPage.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import Title from "./Title";
 import serverUtils from "./serverUtils";
 import CustomButton from "./CustomButton";
+import VisitorDisplay from "./VisitorDisplay";
 
 const fetchPark = async (id) => {
   const response = await fetch(
@@ -19,19 +20,42 @@ const ParkPage = ({ route, navigation }) => {
   const [parkVisitors, setParkVisitors] = useState([]);
 
   useEffect(() => {
-    fetchPark(route.params.id).then((json) => {
-      setParkVisitors(json);
+    fetchPark(route.params.id).then((visitors) => {
+      let visitorDisplayArray = [];
+      for(let name in visitors){
+        let visitorDisplay = <VisitorDisplay visitor_name={name} dog_names={visitors[name]} />
+        visitorDisplayArray.push(visitorDisplay);
+      }
+      setParkVisitors(visitorDisplayArray);
     });
   }, []);
 
   return (
     <View style={styles.container}>
       <Title />
-      <Text>{route.params.name}</Text>
+      <Text style={styles.title}>{route.params.name}</Text>
       <Text>{parkVisitors}</Text>
-      <CustomButton onPress={() => navigation.goBack()} text={"Go back"} />
+      <CustomButton
+        onPress={() => navigation.goBack()}
+        text={"Go back"}
+        bgColor="#000000"
+      />
     </View>
   );
 };
+
+styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  visitorDisplay: {
+    fontSize: 20,
+  },
+  title: {
+    fontSize: 30,
+  },
+});
 
 export default ParkPage;
